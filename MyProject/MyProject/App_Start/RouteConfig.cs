9bc8354c7111ba3f2e7025fed6b0f7b1699dfbcb
Amarnath6111 <amarnathma@infinite.com>
2023-11-23 16:47:48 +0530

    NavbarChanges

--- MyProject/MyProject/App_Start/RouteConfig.cs
+++ MyProject/MyProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ProjectController", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
