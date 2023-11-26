7135ad8a8bdbf464a8767e84cad68ee350f59060
NayanaMuniyappa <144369229+NayanaMuniyappa@users.noreply.github.com>
2023-11-26 22:59:50 +0530

    Final Update

---
+++ MyProject/MyProject/MyProject/Tests/ProjectControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyProject.Controllers;
+using MyProject.Models;
+using NUnit.Framework;
+
+namespace MyProject.Tests.Controllers
+{
+    [TestFixture]
+    public class ProjectControllerTests
+    {
+        private ProjectController _projectController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _projectController = new ProjectController();
+        }
+
+        [Test]
+        public void Index_ReturnsViewResult()
+        {
+            // Act
+            var result = _projectController.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("", result.ViewName);
+        }
+
+        [Test]
+        public void EmployeeLogin_InvalidModel_ReturnsViewResultWithModelError()
+        {
+            // Arrange
+            var invalidModel = new EmployeeDetails { EmpID = 0, Password = "invalid" };
+            _projectController.ModelState.AddModelError("key", "error message");
+
+            // Act
+            var result = _projectController.EmployeeLogin(invalidModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsFalse(_projectController.ModelState.IsValid);
+        }
+
+        [Test]
+        public void EmployeeLogins_InvalidModel_ReturnsViewResultWithModelError()
+        {
+            // Arrange
+            var invalidModel = new EmployeeDetails { EmpID = 0, Password = "" };
+            _projectController.ModelState.AddModelError("key", "error message");
+
+            // Act
+            var result = _projectController.EmployeeLogin(invalidModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsFalse(_projectController.ModelState.IsValid);
+        }
+
+        [Test]
+        public void ManagerLogin_InvalidModel_ReturnsViewResultWithModelError()
+        {
+            // Arrange
+            var invalidModel = new ManagerLogin { ManagerID = 0, Password = "invalid" };
+            _projectController.ModelState.AddModelError("key", "error message");
+
+            // Act
+            var result = _projectController.ManagerLogin(invalidModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsFalse(_projectController.ModelState.IsValid);
+        }
+
+        [Test]
+        public void ManagerLogins_InvalidModel_ReturnsViewResultWithModelError()
+        {
+            // Arrange
+            var invalidModel = new ManagerLogin { ManagerID = 0, Password = "" };
+            _projectController.ModelState.AddModelError("key", "error message");
+
+            // Act
+            var result = _projectController.ManagerLogin(invalidModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsFalse(_projectController.ModelState.IsValid);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // To Clean up any resources
+        }
+    }
+}
